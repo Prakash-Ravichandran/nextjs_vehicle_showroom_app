ec6b1c2d2b92ddbb73fd712bcf608b812d205402
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-03-09 18:43:59 +0530

    [fix]: Hero component layout fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,17 +25,12 @@ export const Hero = () => {
             handleClick={handleScroll}
             btnType={"button"}
           />
-          <div className="hero__image-container">
-            <div className="hero__image">
-              <Image
-                src="/hero.png"
-                alt="hero"
-                fill
-                className="object-contain"
-              />
-              <div className="hero__image-overlay" />
-            </div>
+        </div>
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image src="/hero.png" alt="hero" fill className="object-contain" />
           </div>
+          <div className="hero__image-overlay" />
         </div>
       </div>
     </>
