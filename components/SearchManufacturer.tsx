dfe3dcb8710e67fe6ce5f68da88fd033256591bb
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-03-12 14:45:19 +0530

    [feat]: search functionality selected - statevalues

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -25,7 +25,7 @@ export const SearchManufacturer = ({
   return (
     <>
       <div className="search-manufacturer">
-        <Combobox>
+        <Combobox value={manufacturer} onChange={setManuFacturer}>
           <div className="relative w-full">
             <Combobox.Button className="absolute top-[14px]">
               <Image
