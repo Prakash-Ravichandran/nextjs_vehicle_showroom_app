6617983947e30dcbd865a803fac720e9b1f57bc1
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-03-12 21:41:33 +0530

    [fix]: commented /components

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// import { manufacturers } from "@/constants/constants";
+import { manufacturers } from "@/constants/constants";
 import { SearchManuFacturerProps } from "@/types";
 import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
